8ba1ab406c647cb3493bb21f641ecc41fc4c456e
galdinorosas <dino.rosas@gmail.com>
2016-01-24 11:36:22 +0000

    made changes to only have the first user as the drawers. All subsequent users will be guessers. Have not figured out how to match the drawers word to the guessers word.

--- public/main.js
+++ public/main.js
@@ -3,15 +3,68 @@ var pictionary = function() {
     var socket = io();
     
     var guessBox;
+    var userType;
+    
+    var WORDS = [
+    "word", "letter", "number", "person", "pen", "class", "people",
+    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "brother", "sister", "world", "head", "page", "country", "question",
+    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+    "west", "child", "children", "example", "paper", "music", "river", "car",
+    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+    "space"
+    ];
+    
+    var randomWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+    
+
+    
+    socket.on('userTypeCheck',function(type){
+        
+        userType = type;
+        if(userType===true){
+            $('#top-message .drawerTag').css('display','inline-block');
+            $('.drawerTag span').text(randomWord);
+            $('#top-message #guess').css('display','none');
+        }
+        else if(userType===false){
+            $('#top-message #guess').css('display','inline-block');
+            $('#top-message .drawerTag').css('display','none');
+        }
+        
+    });
+    
+    
+
 
     var onKeyDown = function(event) {
         if (event.keyCode != 13) { // Enter
             return;
         }
+        
+        if(userType===false){
     
-        console.log(guessBox.value());
+        console.log(guessBox.val());
+        
+        socket.emit('guessToServer', {guess:guessBox.val(),random:randomWord});
+
+        
         guessBox.val('');
+        }
+
     };
+    
+    socket.on('guessToClient', function(information){
+        
+        var guessArea = $('.userGuesses');
+        guessArea.text(information.guess);
+    });
     
 
     guessBox = $('input');
@@ -38,12 +91,14 @@ var pictionary = function() {
         drawing = false;
     });
     canvas.on('mousemove', function(event) {
-        if(drawing){
-        var offset = canvas.offset();
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-        draw(position);
-        socket.emit('clientToServer',position);
+        if(userType===true){
+            if(drawing){
+                var offset = canvas.offset();
+                var position = {x: event.pageX - offset.left,
+                                y: event.pageY - offset.top};
+                draw(position);
+                socket.emit('clientToServer',position);
+            }
         }
         
     });
@@ -51,6 +106,8 @@ var pictionary = function() {
     
     
     socket.on('serverToClient', draw);
+    socket.emit('drawerWordCheck',{type:userType,word:randomWord});
+
 };
 
 $(document).ready(function() {
--- server.js
+++ server.js
@@ -9,17 +9,66 @@ var server = http.Server(app);
 var io = socket_io(server);
 
 var totalUsers = 0;
-
+var serverUserType = true;
+var drawerWord;
     io.on('connection', function(socket){
+
         
         totalUsers++;
+        if(totalUsers===1){
+            serverUserType = true;
+        }
+        else if(totalUsers === 2){
+            serverUserType = false;
+        }
+        
+        socket.emit('userTypeCheck', serverUserType);
+
+
+        socket.on('drawerWordCheck', function(wordCheckObject){
+            if(wordCheckObject.type===true){
+                drawerWord=wordCheckObject.word;
+                return drawerWord;
+            }
+            
+        });
+        
+
+        
+        
+
+
         console.log(totalUsers);
+        
+
+
         
         socket.on('clientToServer', function(clientObject){
             socket.broadcast.emit('serverToClient', clientObject);
             
         });
         
+        socket.on('guessToServer', function(message){
+            
+            console.log(message);
+            if(message.guess.toLowerCase()===drawerWord){
+                var serverToClientObject = {guess:message.guess,drawerWord:drawerWord};
+                console.log(serverToClientObject);
+                socket.emit('guessToClient', serverToClientObject);
+                socket.broadcast.emit('guessToClient', serverToClientObject);
+            };
+            socket.broadcast.emit('guessToClient', message);
+
+        });
+        
+
+        
+        console.log(drawerWord);
+        
+        socket.on('disconnect', function(){
+            totalUsers--;
+            console.log(totalUsers);
+        });
         
         
         
