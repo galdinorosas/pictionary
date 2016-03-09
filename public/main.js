4674d61f97d7ec608c53ba29d2efc38d27bcb9bb
galdinorosas <dino.rosas@gmail.com>
2016-03-09 06:19:20 +0000

    made small changes to comments and cleaned up.

--- public/main.js
+++ public/main.js
@@ -1,93 +1,92 @@
 var pictionary = function() {
     var canvas, context;
     var socket = io();
-    
+
     var guessBox;
     var userType;
     var initialGuess;
     var randomWord;
     var ID;
     var guesserCount = 0;
-    
-    socket.on('setID', function(userID){
+
+    socket.on('setID', function(userID) {
         ID = userID;
     });
-    
+
     var WORDS = [
-    "word", "letter", "number", "person", "pen", "class", "people",
-    "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
-    "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
-    "land", "home", "hand", "house", "picture", "animal", "mother", "father",
-    "brother", "sister", "world", "head", "page", "country", "question",
-    "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
-    "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
-    "west", "child", "children", "example", "paper", "music", "river", "car",
-    "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
-    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
-    "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
-    "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
-    "space"
+        "word", "letter", "number", "person", "pen", "class", "people",
+        "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
+        "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
+        "land", "home", "hand", "house", "picture", "animal", "mother", "father",
+        "brother", "sister", "world", "head", "page", "country", "question",
+        "answer", "school", "plant", "food", "sun", "state", "eye", "city", "tree",
+        "farm", "story", "sea", "night", "day", "life", "north", "south", "east",
+        "west", "child", "children", "example", "paper", "music", "river", "car",
+        "foot", "feet", "book", "science", "room", "friend", "idea", "fish",
+        "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+        "body", "dog", "family", "song", "door", "product", "wind", "ship", "area",
+        "rock", "order", "fire", "problem", "piece", "top", "bottom", "king",
+        "space"
     ];
-    
+
     //drawerReset function clears the userGuesses div and displays the drawerTag showing the user it is
     //their turn to draw the displayed word.
-    var drawerReset = function(){
+    var drawerReset = function() {
         randomWord = WORDS[Math.floor(Math.random() * WORDS.length)];
         $('.userGuesses').empty();
-        $('#top-message .drawerTag').css('display','inline-block');
+        $('#top-message .drawerTag').css('display', 'inline-block');
         $('.drawerTag span').text(randomWord);
-        $('#top-message #guess').css('display','none');
-        context.clearRect(0,0,canvas[0].width,canvas[0].height);
+        $('#top-message #guess').css('display', 'none');
+        context.clearRect(0, 0, canvas[0].width, canvas[0].height);
 
     };
-    
+
     //The guesserReset also resets the userGuesses div and displays the make a guess input. This tells the user
     //it is their turn to make a guess.
-    var guesserReset = function(){
+    var guesserReset = function() {
         $('.userGuesses').empty();
-        $('#top-message #guess').css('display','inline-block');
-        $('#top-message .drawerTag').css('display','none');
-        context.clearRect(0,0,canvas[0].width,canvas[0].height);
+        $('#top-message #guess').css('display', 'inline-block');
+        $('#top-message .drawerTag').css('display', 'none');
+        context.clearRect(0, 0, canvas[0].width, canvas[0].height);
     };
 
     //The userTypeCheck listener gets the type parameter from the server and declares the clients userType variable.
     //The server takes the first user as a drawer(a drawer's userType = true), and all subsequent connections
     //are guessers (a guessers userType = false).
-    socket.on('userTypeCheck',function(type){
-        
+    socket.on('userTypeCheck', function(type) {
+
         userType = type;
-        if(userType===true ){
+        if (userType === true) {
             drawerReset();
-        }
-        else if(userType===false){
+        } else if (userType === false) {
             guesserReset();
         }
-        
+
         //This clientToServerWordCheck listener sends an object to the server containing the userType and randomWord.
-        socket.emit('clientToServerWordCheck',{drawer:userType, word:randomWord});
+        socket.emit('clientToServerWordCheck', { drawer: userType, word: randomWord });
 
     });
 
     //The serverToClientDrawerCheck listener will have a function which emits the userType back to the server.
-    socket.on('serverToClientDrawerCheck', function(){
+    socket.on('serverToClientDrawerCheck', function() {
 
-            socket.emit('drawerHere',userType);
+        socket.emit('drawerHere', userType);
     });
-    
+
     //addNewDrawer and resetGuessers are listeners to restart the game and add a new drawer when the previous drawer so happens
     //to leave the game.
-    socket.on('addNewDrawer', function(addDrawer){
+    socket.on('addNewDrawer', function(addDrawer) {
         userType = addDrawer;
         drawerReset();
-        socket.emit('clientToServerWordCheck',{drawer:userType, word:randomWord});
+        socket.emit('clientToServerWordCheck', { drawer: userType, word: randomWord });
 
     });
-    
-    socket.on('resetGuessers', function(){
+
+    socket.on('resetGuessers', function() {
         guesserReset();
-        
+
     });
-    
+
 
     //function for the enter click. This click is only available to the guesser and not the drawer.
     //The initialGuess variable is assigned to the users guess on every enter click. Then the guessToServer
@@ -96,53 +95,52 @@ var pictionary = function() {
         if (event.keyCode != 13) { // Enter
             return;
         }
-        
-        if(userType===false){
+
+        if (userType === false) {
             initialGuess = guessBox.val();
-        
-            socket.emit('guessToServer', {guess:guessBox.val()});
-    
+
+            socket.emit('guessToServer', { guess: guessBox.val() });
+
             guessBox.val('');
         }
 
     };
-    
+
     //This guessToClient listener performs a function with the object created in the guessToServer listener.
-    socket.on('guessToClient', function(information){
-        
+    socket.on('guessToClient', function(information) {
+
         //If the guesser guesses the word correctly then an object is emitted to ALL clients including the client that emited.
         //But if the guesser does not guess the word correctly then a guessToServer listener is broadcasted.
         //If the guessToServer listener is broadcasted then the following if statement will never be true.
-        if(information.guess === initialGuess ){
+        if (information.guess === initialGuess) {
             userType = true;
             drawerReset();
-            context.clearRect(0,0,canvas[0].width,canvas[0].height);
-            socket.emit('clientToServerWordCheck',{drawer:userType, word:randomWord});
+            context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+            socket.emit('clientToServerWordCheck', { drawer: userType, word: randomWord });
 
-        //If the guesser guesses the word correctly, then drawer is found by checking for the userType and
-        //comparing the drawers span context (which is the randomWord) to the initial saved drawerWord. If the guesser
-        //does not guess correctly then the information object will only contain a guess property.
+            //If the guesser guesses the word correctly, then drawer is found by checking for the userType and
+            //comparing the drawers span context (which is the randomWord) to the initial saved drawerWord. If the guesser
+            //does not guess correctly then the information object will only contain a guess property.
+        } else if (userType === true && $('span').text() === information.word) {
+            userType = false;
+            guesserReset();
+            context.clearRect(0, 0, canvas[0].width, canvas[0].height);
         }
-        else if(userType===true && $('span').text() ===information.word){
-                userType = false;
-                guesserReset();
-                context.clearRect(0,0,canvas[0].width,canvas[0].height);
-        }
-        
+
         //If the user guesses correctly then an object will return that contains both the guess and word
         //property. Then this function will reset the canvas and guesser input for all other guessers.
-        else if(userType===false && information.guess === information.word){
+        else if (userType === false && information.guess === information.word) {
             guesserReset();
-            context.clearRect(0,0,canvas[0].width,canvas[0].height);
+            context.clearRect(0, 0, canvas[0].width, canvas[0].height);
         }
         //If the guesser does not guess correctly then the userGuesses div will be changed to the wrong guess to all
         //other clients,excluding the client that sent the guess since the emitter will be broadcasted.
         else {
             var guessArea = $('.userGuesses');
-            guessArea.text(information.guess);   
+            guessArea.text(information.guess);
         }
     });
-    
+
     guessBox = $('input');
     guessBox.on('keydown', onKeyDown);
 
@@ -150,46 +148,47 @@ var pictionary = function() {
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y,
-                         6, 0, 2 * Math.PI);
+            6, 0, 2 * Math.PI);
         context.fill();
     };
+    
     //drawing boolean is used to determine when to draw if the user clicks down.
     var drawing = false;
     canvas = $('canvas');
     context = canvas[0].getContext('2d');
-    
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
+
     //drawing boolean is changed from true to false from the mousedown and mouseup events.
-    canvas.on('mousedown', function(){
+    canvas.on('mousedown', function() {
         drawing = true;
     });
-    canvas.on('mouseup', function(){
+
+    canvas.on('mouseup', function() {
         drawing = false;
     });
+
     canvas.on('mousemove', function(event) {
-        if(userType){
-            if(drawing){
+        if (userType) {
+            if (drawing) {
                 var offset = canvas.offset();
-                var position = {x: event.pageX - offset.left,
-                                y: event.pageY - offset.top};
+                var position = {
+                    x: event.pageX - offset.left,
+                    y: event.pageY - offset.top
+                };
                 draw(position);
-                socket.emit('clientToServer',position);
+                socket.emit('clientToServer', position);
             }
         }
-        
     });
-    
+
     //This serverToClient listener will use the draw function when it is emmited. Which
     //will be in the server due to the mousemove event.
+
     socket.on('serverToClient', draw);
 
-//Make a disconnection function where after every disconnect. All clients are checked to see if the drawer is still connected.
-//If the drawer is not connected then make the last guesser that connected the drawer.
 };
 
 $(document).ready(function() {
     pictionary();
-    
 });
-
--- server.js
+++ server.js
@@ -12,58 +12,49 @@ var totalUsers = 0;
 var previousTotalUsers;
 var serverUserType = true;
 var drawerWord;
-var usersArray =[];
+var usersArray = [];
 var connectedUsersArray;
 var drawerHere = false;
 var drawerCheckCount = 0;
 
-io.on('connection', function(socket){
-    
+io.on('connection', function(socket) {
+
     socket.emit('setID', socket.id);
-    
+
     previousTotalUsers = totalUsers;
-    
+
     totalUsers++;
-    
-    if(totalUsers>1){
+
+    if (totalUsers > 1) {
         serverUserType = false;
-    }
-    else{
+    } else {
         serverUserType = true;
     }
 
     socket.emit('userTypeCheck', serverUserType);
 
-    socket.on('clientToServerWordCheck', function(wordCheckObject){
-        if(wordCheckObject.drawer){
-            drawerWord=wordCheckObject.word;
-            
+    socket.on('clientToServerWordCheck', function(wordCheckObject) {
+        if (wordCheckObject.drawer) {
+            drawerWord = wordCheckObject.word;
         }
-
     });
 
     console.log(totalUsers);
-    
-    socket.on('clientToServer', function(clientObject){
+
+    socket.on('clientToServer', function(clientObject) {
         socket.broadcast.emit('serverToClient', clientObject);
     });
-    
-    socket.on('guessToServer', function(message){
-        
-        if(message.guess.toLowerCase()===drawerWord){
-            var guessToClientObject = {guess:message.guess,word:drawerWord};
+
+    socket.on('guessToServer', function(message) {
+        if (message.guess.toLowerCase() === drawerWord) {
+            var guessToClientObject = { guess: message.guess, word: drawerWord };
             io.sockets.emit('guessToClient', guessToClientObject);
+        } else {
+            socket.broadcast.emit('guessToClient', message);
         }
-        else{
-
-        socket.broadcast.emit('guessToClient', message);
-        
-        }
-        
-
     });
-    
-    socket.on('disconnect', function(){
+
+    socket.on('disconnect', function() {
         totalUsers--;
         usersArray = [io.sockets.clients()];
         connectedUsersArray = Object.getOwnPropertyNames(usersArray[0].server.nsps['/'].sockets);
@@ -71,28 +62,25 @@ io.on('connection', function(socket){
         socket.broadcast.emit('serverToClientDrawerCheck');
 
     });
-   
-    socket.on('drawerHere', function(clientToServerDrawerCheck){
+
+    socket.on('drawerHere', function(clientToServerDrawerCheck) {
         drawerCheckCount++;
         // set drawer exist if any client is a drawer
-        if(clientToServerDrawerCheck === true){
+        if (clientToServerDrawerCheck === true) {
             drawerHere = true;
         }
-        
+
         // if the last client
-        if(drawerCheckCount === totalUsers) {
-        
+        if (drawerCheckCount === totalUsers) {
             // if there is a drawer
-            if(drawerHere===true){
+            if (drawerHere === true) {
                 drawerCheckCount = 0;
-            }
-            else{
+            } else {
                 drawerCheckCount = 0;
-                for(var i = 0; i < connectedUsersArray.length;i++){
-                    if(i === connectedUsersArray.length-1){
+                for (var i = 0; i < connectedUsersArray.length; i++) {
+                    if (i === connectedUsersArray.length - 1) {
                         io.sockets.connected[connectedUsersArray[i]].emit('addNewDrawer', true);
-                    }
-                    else{
+                    } else {
                         io.sockets.connected[connectedUsersArray[i]].emit('resetGuessers');
                     }
                 }
@@ -100,9 +88,7 @@ io.on('connection', function(socket){
             
             drawerHere = false;
         }
-        
     });
-    
 });
 
 server.listen(8080);
